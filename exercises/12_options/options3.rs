7f65e602ab1a7403e24b98210f5add9eaa5adc96
Eddy3129 <eddyfy2003@gmail.com>
2025-12-16 22:48:14 +0800

    completed till exercise 18

---
+++ exercises/12_options/options3.rs
@@ -0,0 +1,16 @@
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+fn main() {
+    let optional_point = Some(Point { x: 100, y: 200 });
+
+    // TODO: Fix the compiler error by adding something to this match statement.
+    if let Some(p) = &optional_point {
+        println!("Coordinates are {},{}", p.x, p.y);
+    }
+
+    println!("{optional_point:?}"); // Don't change this line.
+}
